cd6aa606364c491238048d3192bdb3b9c50de683
Amir Fleminger <amirfl@yahoo.com>
2015-10-24 19:29:02 -0400

    Initial Commit

---
+++ testingRealm/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testingRealm
+//
+//  Created by Amir Fleminger on 10/24/15.
+//  Copyright © 2015 Amir Fleminger-Fleminger Media LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ testingRealm/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  testingRealm
+//
+//  Created by Amir Fleminger on 10/24/15.
+//  Copyright © 2015 Amir Fleminger-Fleminger Media LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
